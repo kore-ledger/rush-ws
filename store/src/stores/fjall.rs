4efdb0f152405824214f225a97f351fb4b92e05f
Antonio Estevez <aestgar62gmail.com>
2026-03-13 11:33:30 +0000

    test: fjall macro test

--- store/src/stores/fjall.rs
+++ store/src/stores/fjall.rs
@@ -66,14 +66,29 @@ impl FjallStore {
 
     pub fn into_key_value(guard: Guard) -> Result<(u64, Vec<u8>), Error> {
         let (key, value) = guard.into_inner()
-            .map_err(|e| Error::Reading(format!("failed to read key/value from fjall -> {}", e)))?;
+            .map_err(|e| Error::Reading(
+                format!("failed to read key/value from fjall -> {}", e)
+            ))?;
         let key_str = String::from_utf8_lossy(&key);
         let key_num = key_str.split(':').last()
             .and_then(|s| s.parse::<u64>().ok())
-            .ok_or_else(|| Error::Reading(format!("failed to parse key from fjall -> {}", key_str)))?;
+            .ok_or_else(|| Error::Reading(
+                format!("failed to parse key from fjall -> {}", key_str)
+            ))?;
         Ok((key_num, value.to_vec()))
     }
 
+    /// Returns the last key in the store that matches the prefix. The keys are parsed as u64 
+    /// values. If there is an error in parsing a key, it is skipped. If there are no keys that
+    /// match the prefix, it returns 0.
+    /// 
+    /// This method is useful for determining the highest key currently stored in the Fjall 
+    /// database for the given prefix.
+    /// 
+    /// # Returns
+    /// 
+    /// - `u64`: The last key in the store that matches the prefix, or 0 if there are no matching 
+    ///    keys.
     pub fn last_key(&self) -> u64 {
         let iter = self.keyspace.prefix(&self.prefix);
         iter.last()
@@ -81,6 +96,15 @@ impl FjallStore {
                 Self::into_key_value(guard).ok().map(|(key, _)| key)
             })
             .unwrap_or(0)
+    }
+
+    /// Returns an iterator over all key-value pairs in the store that match the prefix. The keys 
+    /// are parsed as u64 values, and the values are returned as byte vectors. If there is an 
+    /// error in parsing a key, that entry is skipped.
+    pub fn prefix_iterator<'a>(&'a self) -> impl Iterator<Item = (u64, Vec<u8>)> + 'a {
+        self.keyspace.prefix(&self.prefix).filter_map(|guard| {
+            Self::into_key_value(guard).ok()
+        })
     }
 }
 
@@ -148,7 +172,7 @@ impl Store for FjallStore {
         &'a self,
         options: IteratorOptions,
     ) -> Box<dyn Iterator<Item = (u64, Vec<u8>)> + 'a> {
-        let vec = self.keyspace.prefix(&self.prefix).collect::<Vec<_>>();
+        let vec = self.prefix_iterator().collect::<Vec<_>>();
         let size = vec.len() as u64;
         match options {
             IteratorOptions::Forward { from, count } => {
@@ -156,9 +180,8 @@ impl Store for FjallStore {
                 let from = from.unwrap_or(0);
                 let count = count.unwrap_or(size);
                 Box::new(
-                    vec.into_iter().filter_map(move |guard| {
-                        let (key, value) = Self::into_key_value(guard).ok()?;
-                        if key <= from {
+                    vec.into_iter().filter_map(move |(key, value)| {
+                        if key < from {
                             return None;
                         }
                         Some((key, value))
@@ -171,9 +194,8 @@ impl Store for FjallStore {
                 let from = from.unwrap_or(self.last_key());
                 let count = count.unwrap_or(size);
                 Box::new(
-                    vec.into_iter().rev().filter_map(move |guard| {
-                        let (key, value) = Self::into_key_value(guard).ok()?;
-                        if key >= from {
+                    vec.into_iter().rev().filter_map(move |(key, value)| {
+                        if key > from {
                             return None;
                         }
                         Some((key, value))
@@ -185,8 +207,7 @@ impl Store for FjallStore {
                 // Implement range iteration logic here
                 let to = to.unwrap_or(self.last_key());
                 Box::new(
-                    vec.into_iter().filter_map(move |guard| {
-                        let (key, value) = Self::into_key_value(guard).ok()?;
+                    vec.into_iter().filter_map(move |(key, value)| {
                         if key >= from && key <= to {
                             Some((key, value))
                         } else {
@@ -217,21 +238,6 @@ mod tests {
         assert!(store.is_ok());
     }
 
-    #[test]
-    fn test_fjall_store_put_get() {
-        //let manager = FjallDbManager::default();
-        let temp_dir = TempDir::new().unwrap();
-        let binding = temp_dir.path();
-        let path = binding.to_str().unwrap();
-        let manager = FjallDbManager::new(path);
-        let manager = manager.unwrap();
-
-        let mut store = manager.create_store("test", "test").unwrap();
-        store.put(1, b"hello").unwrap();
-        let value = store.get(1).unwrap();
-        assert_eq!(value, b"hello");
-    }
-
     impl Default for FjallDbManager {
         fn default() -> Self {
             let path = PathBuf::from("./db/fjall_db");
@@ -240,6 +246,8 @@ mod tests {
                 .expect("Failed to create FjallDbManager")
        }
     } 
+ 
+
     test_store_trait!{
         unit_test_fjall_store:crate::stores::fjall::FjallDbManager:crate::stores::fjall::FjallStore
     }
